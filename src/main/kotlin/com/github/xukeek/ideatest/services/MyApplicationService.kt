60854aa2a79db0e06944d0ba36acac7959acba65
GitHub Action <action@github.com>
2021-10-25 02:25:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/xukeek/ideatest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xukeek/ideatest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xukeek.ideatest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xukeek.ideatest.MyBundle
 
 class MyApplicationService {
 
